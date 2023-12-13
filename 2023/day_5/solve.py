2cfcf34d8e788241dd671fd682742693e1c63ce0
Gedas <gediminas.repecka@gmail.com>
2023-12-13 15:33:50 +0200

    FINALLY Asjkhasdfkljshadfljkn

--- 2023/day_5/solve.py
+++ 2023/day_5/solve.py
@@ -141,91 +141,167 @@ def get_min_location(seeds, mappings):
 seeds = get_seed_range_list(INPUT)
 
 
-def split_range_by_mapping(seed_range, mapping):
-    range_start, range_end = seed_range
+def is_all_range_unmapped(r, map):
+    range_start, range_end = r
     range_start = int(range_start)
     range_end = int(range_end)
+    dst_start, src_start, range_len = map
+    dst_start = int(dst_start)
+    src_start = int(src_start)
+    src_end = int(src_start) + int(range_len) - 1
+    src_end = int(src_end)
+    dst_end = int(dst_start) + int(range_len) - 1
+    dst_end = int(dst_end)
 
+    if ((range_start < src_start and range_end < src_start) or
+            (range_end > src_end and range_start > src_end)):
+        curr_range = (range_start, range_end)
+        return curr_range
+
+
+def is_all_range_mapped(r, map):
+    range_start, range_end = r
+    range_start = int(range_start)
+    range_end = int(range_end)
+    dst_start, src_start, range_len = map
+    dst_start = int(dst_start)
+    src_start = int(src_start)
+    src_end = int(src_start) + int(range_len) - 1
+    src_end = int(src_end)
+    dst_end = int(dst_start) + int(range_len) - 1
+    dst_end = int(dst_end)
+
+    if (src_start <= range_start <= src_end and
+            src_start <= range_end <= src_end):
+        mapped_range_start = range_start - src_start + dst_start
+        mapped_range_end = range_end - src_start + dst_start
+        curr_range = (mapped_range_start, mapped_range_end)
+        return curr_range
+
+
+def is_start_range_mapped(r, map, only_mapped=True):
+    range_start, range_end = r
+    range_start = int(range_start)
+    range_end = int(range_end)
+    dst_start, src_start, range_len = map
+    dst_start = int(dst_start)
+    src_start = int(src_start)
+    src_end = int(src_start) + int(range_len) - 1
+    src_end = int(src_end)
+    dst_end = int(dst_start) + int(range_len) - 1
+    dst_end = int(dst_end)
+
+    if (range_start >= src_start and
+            range_start <= src_end and
+            range_end > src_end):
+
+        mapped_range_start = range_start - src_start + dst_start
+        mapped_range_end = dst_end
+        range_1 = (mapped_range_start, mapped_range_end)
+
+        unmapped_range_start = src_end + 1
+        unmapped_range_end = range_end
+        range_2 = (unmapped_range_start, unmapped_range_end)
+
+        if only_mapped:
+            return range_1
+        else:
+            return range_2
+
+
+def is_end_range_mapped(r, map, only_mapped=True):
+    range_start, range_end = r
+    range_start = int(range_start)
+    range_end = int(range_end)
+    dst_start, src_start, range_len = map
+    dst_start = int(dst_start)
+    src_start = int(src_start)
+    src_end = int(src_start) + int(range_len) - 1
+    src_end = int(src_end)
+    dst_end = int(dst_start) + int(range_len) - 1
+    dst_end = int(dst_end)
+
+    if (range_start < src_start and
+            range_end >= src_start and
+            range_end < src_end):
+        unmapped_range_start = range_start
+        unmapped_range_end = src_start - 1
+        range_1 = (unmapped_range_start, unmapped_range_end)
+
+        mapped_range_start = dst_start
+        mapped_range_end = range_end - src_start + dst_start
+        range_2 = (mapped_range_start, mapped_range_end)
+
+        if only_mapped:
+            return range_2
+        else:
+            return range_1
+
+
+def split_range_by_mapping(seed_range, mapping):
     ranges = list()
+
+    unmapped = list()
     for map in mapping:
-        print(f'current map: {map}')
-        dst_start, src_start, range_len = map
-
-        dst_start = int(dst_start)
-        src_start = int(src_start)
-
-        src_end = int(src_start) + int(range_len) - 1
-        src_end = int(src_end)
-
-        dst_end = int(dst_start) + int(range_len) - 1
-        dst_end = int(dst_end)
+       #print(f'current map: {map}')
 
         # all range within mapping
-        if (src_start <= range_start <= src_end and
-                src_start <= range_end <= src_end):
-            mapped_range_start = range_start - src_start + dst_start
-            mapped_range_end = range_end - src_start + dst_start
-            curr_range = (mapped_range_start, mapped_range_end)
-
+        curr_range = is_all_range_mapped(seed_range, map)
+        if curr_range:
             if (curr_range not in ranges):
                 ranges.append(curr_range)
-                print(f'all range within mapping, produced: {curr_range}')
-            continue
-
+               #print(f'all range within mapping, produced: {curr_range}')
 
         # only end of the range included in mapping
-        if (range_start < src_start and
-                range_end >= src_start and
-                range_end < src_end):
-            unmapped_range_start = range_start
-            unmapped_range_end = src_start - 1
-            range_1 = (unmapped_range_start, unmapped_range_end)
+        curr_range = is_end_range_mapped(seed_range, map)
+        curr_range_unmapped = is_end_range_mapped(seed_range, map, False)
+        if curr_range:
+            if (curr_range not in ranges):
+                ranges.append(curr_range)
+               #print(f'only end of the range in mapping, produced: {curr_range}')
 
-            mapped_range_start = dst_start
-            mapped_range_end = range_end - src_start + dst_start
-            range_2 = (mapped_range_start, mapped_range_end)
-            print(f'only end of the range in mapping, produced: {range_1}, {range_2}')
-
-            if (range_1 not in ranges):
-                ranges.append(range_1)
-                print(f'only end of the range in mapping, produced: {range_1}')
-            if (range_2 not in ranges):
-                ranges.append(range_2)
-                print(f'only end of the range in mapping, produced: {range_2}')
+        if curr_range_unmapped:
+            if (curr_range_unmapped not in unmapped):
+                unmapped.append(curr_range_unmapped)
 
         # only start of the range included in mapping
-        if (range_start >= src_start and
-                range_start <= src_end and
-                range_end > src_end):
-
-            mapped_range_start = range_start - src_start + dst_start
-            mapped_range_end = dst_end
-            range_1 = (mapped_range_start, mapped_range_end)
-
-            unmapped_range_start = src_end + 1
-            unmapped_range_end = range_end
-            range_2 = (unmapped_range_start, unmapped_range_end)
-            print(f'only start of the range in mapping, produced: {range_1}, {range_2}')
-
-            if (range_1 not in ranges):
-                ranges.append(range_1)
-                print(f'only start of the range in mapping, produced: {range_1}')
-            if (range_2 not in ranges):
-                ranges.append(range_2)
-                print(f'only start of the range in mapping, produced: {range_2}')
-        # range outside of mapping
-        if ((range_start < src_start and range_end < src_start) or (range_end > src_end and range_start > src_end)):
-            curr_range = (range_start, range_end)
-
+        curr_range = is_start_range_mapped(seed_range, map)
+        curr_range_unmapped = is_start_range_mapped(seed_range, map, False)
+        if curr_range:
             if (curr_range not in ranges):
-                if(curr_range == (range_start, range_end) and len(ranges) > 0):
-                    continue
+                ranges.append(curr_range)
+               #print(f'only start of the range in mapping, produced: {curr_range}')
+
+        if curr_range_unmapped:
+            if (curr_range_unmapped not in unmapped):
+                unmapped.append(curr_range_unmapped)
+
+        # range outside of mapping
+        curr_range = is_all_range_unmapped(seed_range, map)
+        if curr_range:
+            if (curr_range not in ranges):
+                if(curr_range == seed_range and len(ranges) > 0):
+                    pass
                 else:
                     ranges.append(curr_range)
-                    print(f'all range outside mapping, produced: {curr_range}')
-            continue
+                   #print(f'all range outside mapping, produced: {curr_range}')
 
-    if len(ranges) > 1 and ranges[0] == (range_start, range_end):
+        #print(f'unmapped before: {unmapped}')
+        for um in unmapped:
+            #print(f'{um}')
+            for map in mapping:
+                #print(f'\t checking unmapped range: {um}, current map: {map}')
+                curr_range = is_all_range_mapped(um, map)
+                if curr_range:
+                    #print(f'\t deleting: {um} from: {unmapped}')
+                    unmapped.remove(um)
+        #print(f'unmapped after: {unmapped}')
+
+        for uml in unmapped:
+            if uml not in ranges:
+                ranges.append(uml)
+
+    if len(ranges) > 1 and ranges[0] == (seed_range):
         return ranges[1:]
     else:
         return ranges
@@ -237,26 +313,26 @@ def get_seeds_to_check(seeds_ranges, mappings):
     for r in seeds_ranges:
         new_ranges = [r]
         for mapping in mappings:
-            print(f'START: mapping ranges: {new_ranges}')
+           #print(f'START: mapping ranges: {new_ranges}')
             temp = list()
             for nr in new_ranges:
-                print(f'mapping range: {nr}')
+               #print(f'mapping range: {nr}')
                 new_range = split_range_by_mapping(nr, mapping)
                 for n in new_range:
                     temp.append(n)
-                print(f'produced new range: {new_range}')
+               #print(f'produced new range: {new_range}')
             new_ranges = []
             for t in temp:
                 new_ranges.append(t)
-            print(f'DONE: produced new ranges: {new_ranges}\n')
+           #print(f'DONE: produced new ranges: {new_ranges}\n')
         ranges.append(new_ranges)
 
         for r in ranges:
             for range_tuple in r:
                 min_seeds.append(range_tuple[0])
 
-    print(ranges)
-    print(min_seeds)
+   #print(ranges)
+   #print(min_seeds)
     return min_seeds
 
 
