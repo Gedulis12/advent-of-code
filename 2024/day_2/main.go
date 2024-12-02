8dcd749807a95a290989acd27f6ac449e70c5d44
Gediminas Repecka <gediminas.repecka@gmail.com>
2024-12-02 14:22:53 +0200

    2024 day 2

---
+++ 2024/day_2/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println(SolvePart1("input"))
+	fmt.Println(SolvePart2("input"))
+}
+
+func SolvePart1(inputPath string) int {
+	file, err := os.Open(inputPath)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	ans := 0
+	for scanner.Scan() {
+
+		report := strings.Split(scanner.Text(), " ")
+		reportInts := sliceToInts(report)
+
+		if (checkSafeAsc(reportInts) || checkSafeDsc(reportInts)) {
+			ans += 1
+		}
+	}
+	return ans
+}
+
+func SolvePart2(inputPath string) int {
+	file, err := os.Open(inputPath)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	ans := 0
+	for scanner.Scan() {
+
+		report := strings.Split(scanner.Text(), " ")
+		reportInts := sliceToInts(report)
+		damps := getDamps(reportInts)
+
+		if checkSafeAsc(reportInts) || checkSafeDsc(reportInts) || checkDamps(damps) {
+			ans += 1
+		}
+	}
+	return ans
+}
+
+func sliceToInts(report []string) []int {
+	reportInts := []int{}
+	for i, _ := range report {
+		a, err := strconv.Atoi(report[i])
+		if err != nil {
+			panic(err)
+		}
+
+		reportInts = append(reportInts, a)
+	}
+	return reportInts
+}
+
+func getDamps(report []int) [][]int {
+	dampenedReports := [][]int{}
+	for i, _ := range(report) {
+		damp := make([]int, len(report))
+		copy(damp, report)
+		damp = append(damp[:i], damp[i+1:]...)
+		dampenedReports = append(dampenedReports, damp)
+	}
+
+	return dampenedReports
+}
+
+func checkDamps(damps [][]int) bool {
+	for _, v := range(damps) {
+		if checkSafeAsc(v) || checkSafeDsc(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func checkSafeAsc(report []int) bool {
+	checked := 0
+		for i, _ := range(report) {
+			if i == 0 {
+				continue
+			}
+			diff := report[i] - report[i-1]
+			if diff > 0 && diff <= 3 {
+				checked += 1
+			}
+			if checked == len(report)-1 {
+				return true
+			}
+		}
+		return false
+}
+
+func checkSafeDsc(report []int) bool {
+	checked := 0
+		for i, _ := range(report) {
+			if i == 0 {
+				continue
+			}
+			diff := report[i-1] - report[i]
+			if diff > 0 && diff <= 3 {
+				checked += 1
+			}
+			if checked == len(report)-1 {
+				return true
+			}
+		}
+		return false
+}
---
+++ 2024/day_2/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestPart1(t *testing.T) {
+    input := "example"
+    want := 2
+    res := SolvePart1(input)
+    if want != res {
+        t.Fatalf("Expected: %d, Got: %d ", want, res)
+    }
+	t.Logf("Expected: %d, Got: %d ", want, res)
+}
+
+func TestPart2(t *testing.T) {
+    input := "example"
+    want := 4
+    res := SolvePart2(input)
+    if want != res {
+        t.Fatalf("Expected: %d, Got: %d ", want, res)
+    }
+	t.Logf("Expected: %d, Got: %d ", want, res)
+}
